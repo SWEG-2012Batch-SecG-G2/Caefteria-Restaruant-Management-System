8d8b028331775dd49ddd51738b5ff7464765cab2
Yabsera-Haile <83125525+Yabsera-Haile@users.noreply.github.com>
2021-08-24 21:23:40 +0300

    this is a test run

---
+++ program.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string.h>
+#include<math.h>
+using namespace std;
+
+struct stock
+{ 
+   char item_name[100];
+   float price;
+   float amount;
+   
+};
+struct menu
+{
+   char food_name[100];
+   float price;
+   stock ingredient;
+};
+int main()
+{
+     cout<<"Hello World";
+}
